d897df39a580f82e63e4aab68ba8b49e2e0425ee
huangxulei <pjhxl@qq.com>
2024-03-01 16:18:25 +0800

    31

--- test.py
+++ test.py
@@ -1,69 +1,104 @@
 import flet as ft
 
-audioListShown = False
+# 用于是否显示右边栏的辅助变量
+rightFlag = False
 
 
 def main(page: ft.Page):
-    global audioListShown
+    page.title = "上中下布局"
+    page.padding = 0
+    page.window_width = 500
+    page.window_height = 600
 
-    def showAudioList(e):
-        global audioListShown
-        audioList_menu.offset = ft.transform.Offset(0, 0)
-        audioListShown = True
-        audioList_menu.update()
-
-    def hideAudioList(e):
-        global audioListShown
-        audioList_menu.offset = ft.transform.Offset(2, 0)
-        audioListShown = False
-        audioList_menu.update()
-
-    # 播放列表
-    def audioListCtrl(e):
-        if audioListShown == False:
-            showAudioList(0)
-        elif audioListShown == True:
-            hideAudioList(0)
-
-    audioList_btn = ft.IconButton(
-        icon=ft.icons.LIBRARY_MUSIC_OUTLINED,
-        icon_size=20,
-        on_click=audioListCtrl,
+    top = ft.Container(
+        bgcolor=ft.colors.AMBER,
+        height=100,
+        border=ft.border.only(bottom=ft.border.BorderSide(2, "black")),
     )
-    songlist_tiles = ft.Column(
-        controls=[], height=380, spacing=0, scroll=ft.ScrollMode.AUTO
+    crow = ft.Row(
+        controls=[ft.Text("中间自适应高度,设置expand=1")],
+        alignment="center",
     )
-    audioList_menu = ft.Container(
-        content=ft.Column(
-            controls=[
-                ft.Row(
-                    controls=[
-                        ft.Text(
-                            value="播放列表",
-                            size=20,
-                            weight=ft.FontWeight.BOLD,
-                        ),
-                        ft.IconButton(icon=ft.icons.CLOSE, on_click=hideAudioList),
-                    ],
-                    alignment=ft.MainAxisAlignment.SPACE_BETWEEN,
-                ),
-                songlist_tiles,
-            ],
-        ),
+    center = ft.Container(
+        content=crow,
+        expand=1,
+        # 全部边框
+        border=ft.border.all(10, ft.colors.PINK_600),
+    )
+
+    top_widget = ft.Container(
+        bgcolor=ft.colors.YELLOW,
+        content=ft.Text("这是一段文件"),
+        height=page.window_height - 130,
+        width=page.window_width,
+        expand=1,
+        left=0,
+        top=0,
+        border=ft.border.all(2, ft.colors.BLUE_100),
+        visible=False,
+    )
+
+    def showTop(e):
+        if top_widget.visible:
+            top_widget.visible = False
+        else:
+            top_widget.visible = True
+        page.update()
+
+    right_width = ft.Container(
+        bgcolor=ft.colors.GREEN_100,
+        width=100,
+        height=300,
+        content=ft.Text("右边栏"),
+        offset=ft.transform.Offset(4, 0),
+        bottom=100,
         right=10,
-        bottom=65,
-        width=300,
-        height=350,
-        bgcolor=ft.colors.SURFACE_VARIANT,
-        border_radius=6,
-        padding=8,
-        offset=ft.transform.Offset(2, 0),
-        animate_offset=ft.animation.Animation(300, ft.AnimationCurve.EASE_IN_OUT_CUBIC),
     )
 
-    page.overlay.append(audioList_menu)
+    page.overlay.append(right_width)
 
-    page.add(audioList_btn)
+    def showRight(e):
+        global rightFlag
+        if rightFlag:
+            print("隐藏")
+            rightFlag = False
+            right_width.offset = ft.transform.Offset(4, 0)
+        else:
+            print("显示")
+            rightFlag = True
+            right_width.offset = ft.transform.Offset(0, 0)
+        right_width.update()
+
+    top_btn = ft.IconButton(
+        icon=ft.icons.ARROW_DROP_UP,
+        icon_size=20,
+        tooltip="显示上面一个布局",
+        on_click=showTop,
+    )
+
+    right_btn = ft.IconButton(
+        icon=ft.icons.ARROW_RIGHT,
+        icon_size=20,
+        tooltip="显示右边一个布局",
+        on_click=showRight,
+    )
+
+    bottom = ft.Container(
+        bgcolor=ft.colors.GREY_100,
+        height=100,
+        # 设置顶部边框颜色和宽度
+        border=ft.border.only(top=ft.border.BorderSide(2, "black")),
+        content=ft.Row(controls=[top_btn, right_btn]),
+    )
+
+    col = ft.Column([top, center, bottom], expand=True, spacing=0)
+
+    st = ft.Stack(
+        controls=[col, top_widget],
+        expand=True,
+    )
+
+    page.add(st)
 
 
 ft.app(target=main)
